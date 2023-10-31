70668cd0bb6a6dc3ec46014ecee31190c8ce62b1
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2023-10-30 20:32:32 -0400

    Update book-now.js

--- book-now.js
+++ book-now.js
@@ -4,6 +4,12 @@ const hotelTab = document.querySelector('#hotel-tab')
 const cruiseSearch = document.querySelector('.cruise-search')
 const airSearch = document.querySelector('.air-search')
 const hotelSearch = document.querySelector('.hotel-search')
+const roundTrip = document.querySelector('#round-trip')
+const oneWay = document.querySelector('#one-way')
+const multiCity = document.querySelector('#multi-city')
+const returnTrip = document.querySelectorAll('.return-trip')
+const selectionGroupOne = document.querySelector('#selection-group-one')
+const selectionGroupTwo = document.querySelector('#selection-group-two')
 
 cruiseTab.addEventListener('click', () => {
     airSearch.classList.add('inactive')
@@ -28,6 +34,8 @@ roundTrip.addEventListener('click', () => {
     returnTrip.forEach((box) => {
         box.classList.remove('inactive')
     })
+    selectionGroupOne.classList.remove('inactive')
+    selectionGroupTwo.classList.remove('inactive')
 })
 
 oneWay.addEventListener('click', () => {
@@ -37,10 +45,14 @@ oneWay.addEventListener('click', () => {
     returnTrip.forEach((box) => {
         box.classList.add('inactive')
     })
+    selectionGroupOne.classList.remove('inactive')
+    selectionGroupTwo.classList.remove('inactive')
 })
 
 multiCity.addEventListener('click', () => {
     roundTrip.classList.remove('active-type')
     oneWay.classList.remove('active-type')
     multiCity.classList.add('active-type')
+    selectionGroupOne.classList.add('inactive')
+    selectionGroupTwo.classList.add('inactive')
 })
